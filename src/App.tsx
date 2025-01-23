1242acc6e9485ba9406106e72cf5e4e537ce24af
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2025-01-23 23:20:32 +0100

    Area for text input + writing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-// import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  return <div contentEditable="true">Hello, World!</div>;
+  const [text, setText] = useState("");
+  // const [blocks, setBlocks] = useState([]);
+  const editorRef = useRef(null);
+
+  // Updates text in editor using ref
+  function handleTextChange(){
+    if (editorRef.current) {
+      setText(editorRef.current.innerText);
+    }
+  }
+
+  return (
+    <>
+      <div className="editor">
+        <div 
+          className="editable-area"
+          ref={editorRef}
+          contentEditable="true" 
+          suppressContentEditableWarning={true}
+          onInput={handleTextChange}>
+            {/* This is where the text is */}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
+
