7b4a610f53b30f07362e32c10e65dd88b06c13e0
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2025-01-31 13:29:23 +0100

    Basic block making + caret manipulation

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,39 @@ import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const [text, setText] = useState("");
-  // const [blocks, setBlocks] = useState([]);
+  // const [text, setText] = useState("");
+  const [blocks, setBlocks] = useState([]);
   const editorRef = useRef(null);
 
   // Updates text in editor using ref
-  function handleTextChange(){
+  function handleTextChange(e){
     if (editorRef.current) {
-      setText(editorRef.current.innerText);
+      // setText(editorRef.current.innerHTML);
+      // setBlocks(editorRef.current.innerText.split(' '));
+    }
+  }
+
+  function handleKeyDown(e){
+    if(e.key === " "){
+      const lastCaretPosition = window.getSelection()?.focusOffset;
+
+      if (editorRef.current) {
+        let last = editorRef.current.innerText.split(' ')
+        
+        editorRef.current.innerHTML += `<span class='block'>${last}</span>`;
+
+        console.log(editorRef.current.innerText)
+
+
+        // Set caret (text cursor) position
+        let textNode = document.querySelector('.editable-area')?.firstChild;
+        let range = document.createRange();
+        range.setStart(textNode, lastCaretPosition);
+        range.setEnd(textNode, lastCaretPosition);
+        let sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+      }
     }
   }
 
@@ -21,8 +46,17 @@ function App() {
           ref={editorRef}
           contentEditable="true" 
           suppressContentEditableWarning={true}
-          onInput={handleTextChange}>
+          onInput={handleTextChange}
+          onKeyDown={handleKeyDown}
+          >
             {/* This is where the text is */}
+        </div>
+        <div>
+          {/* {
+            blocks.map(block => {
+              return <span className="block">{block}</span>
+            })
+          } */}
         </div>
       </div>
     </>
