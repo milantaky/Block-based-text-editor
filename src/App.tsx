2b6ea591be5d546059fc8fdc593da4b2ce0db0ef
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2025-02-03 13:33:24 +0100

    text/block mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,57 @@ import "./App.css";
 
 function App() {
   // const [text, setText] = useState("");
+  const [textMode, setTextMode] = useState(true);
   const [blocks, setBlocks] = useState([]);
   const editorRef = useRef(null);
 
   // Updates text in editor using ref
-  function handleTextChange(e){
-    if (editorRef.current) {
-      // setText(editorRef.current.innerHTML);
-      // setBlocks(editorRef.current.innerText.split(' '));
+  // function handleTextChange(e){
+  //   if (editorRef.current) {
+  //     // setText(editorRef.current.innerHTML);
+  //     // setBlocks(editorRef.current.innerText.split(' '));
+  //   }
+  // }
+
+  function Block({ content }){
+    return <span className="block">{content}</span>;
+  }
+
+  function setCaretPosition(position: number) {
+    const textNode = document.querySelector('.editable-area');
+    const range = document.createRange();
+    range.setStart(textNode, position);
+    range.setEnd(textNode, position);
+    const sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+
+  function convertToBlock(){
+
+    const lastCaretPosition = window.getSelection()?.focusOffset;
+
+    if(editorRef.current){
+      let last = editorRef.current.innerText.split(' ').pop();
+      
+      editorRef.current.innerHTML += `<span class='block'>${last}</span>`;
+      
+      console.log(editorRef.current.innerText)
+
+      setCaretPosition(lastCaretPosition);
     }
   }
 
   function handleKeyDown(e){
     if(e.key === " "){
-      const lastCaretPosition = window.getSelection()?.focusOffset;
-
-      if (editorRef.current) {
-        let last = editorRef.current.innerText.split(' ')
-        
-        editorRef.current.innerHTML += `<span class='block'>${last}</span>`;
-
-        console.log(editorRef.current.innerText)
-
-
-        // Set caret (text cursor) position
-        let textNode = document.querySelector('.editable-area')?.firstChild;
-        let range = document.createRange();
-        range.setStart(textNode, lastCaretPosition);
-        range.setEnd(textNode, lastCaretPosition);
-        let sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
+      if(!textMode){
+        convertToBlock();
       }
     }
+  }
+
+  function handleButtonClick(){
+    setTextMode(!textMode);
   }
 
   return (
@@ -46,7 +64,7 @@ function App() {
           ref={editorRef}
           contentEditable="true" 
           suppressContentEditableWarning={true}
-          onInput={handleTextChange}
+          // onInput={handleTextChange}
           onKeyDown={handleKeyDown}
           >
             {/* This is where the text is */}
@@ -58,10 +76,13 @@ function App() {
             })
           } */}
         </div>
+        <div>
+          <button onClick={handleButtonClick}>Switch Modes</button>
+          {(textMode) ? "text" : "blocks"}
+        </div>
       </div>
     </>
   );
 }
 
 export default App;
-
