de887e8590d98e1ef098d079b83fbe6bee0503e3
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2025-04-16 14:44:59 +0200

    Removed the line solution from before, letting it overflow, implemented moving in wrapped line

--- src/BlockEditor.tsx
+++ src/BlockEditor.tsx
@@ -56,18 +56,18 @@ export default function BlockEditor({
     // console.log(blocks)
   }, [blocks]);
 
-  // Check for line-wrapping
-  useEffect(() => {
-    let changed = false;
+  //   // Check for line-wrapping
+  //   useEffect(() => {
+  //     let changed = false;
 
-    if (checkLineHeight(inputLineIndex)) {
-      changed = true;
-    }
+  //     if (checkLineHeight(inputLineIndex)) {
+  //       changed = true;
+  //     }
 
-    if (!changed && inputLineIndex > 0 && checkLineHeight(inputLineIndex - 1)) {
-      changed = true;
-    }
-  }, [blocks, inputText]);
+  //     if (!changed && inputLineIndex > 0 && checkLineHeight(inputLineIndex - 1)) {
+  //       changed = true;
+  //     }
+  //   }, [blocks, inputText]);
 
   function convertToBlocks(text: string) {
     if (text === "") return [];
@@ -75,14 +75,14 @@ export default function BlockEditor({
   }
 
   // Checks if line has wrapped
-  // When adding new line, \r get set as line divider, not \n!! 
+  // When adding new line, \r get set as line divider, not \n!!
   // It is because there wouln't be a way to know if the line below is made to prevent wrapping or if it is a new line on purpose
   function checkLineHeight(line: number) {
     const domLines = document.getElementsByClassName("line");
 
     // Is checked line higher than base line
     if (domLines[line].scrollHeight <= baseLineHeight.current) {
-        return false;
+      return false;
     }
 
     // If there already is new line, do nothing (prevents endless cycle of new lines)
@@ -92,21 +92,23 @@ export default function BlockEditor({
     }
 
     // Input not on end -> move last block to next line
-    if(inputIndex !== lines[inputLineIndex].length){
-
-        // Is there already a wrapped line ("\r")? -> Swap "\r" with element before
-        const possibleWrapIndex = insertIndex - inputIndex + lines[inputLineIndex].length;
-        if (blocks[possibleWrapIndex] === "\r"){
-            const newBlocks = [...blocks];
-            [newBlocks[possibleWrapIndex], newBlocks[possibleWrapIndex - 1]] = [newBlocks[possibleWrapIndex - 1], newBlocks[possibleWrapIndex]];
-            setBlocks(newBlocks);
-            return true;
-        } else {
-            addNewLine(possibleWrapIndex - 1, false, true);
-            return true;
-        }
-    }   
-
+    if (inputIndex !== lines[inputLineIndex].length) {
+      // Is there already a wrapped line ("\r")? -> Swap "\r" with element before
+      const possibleWrapIndex =
+        insertIndex - inputIndex + lines[inputLineIndex].length;
+      if (blocks[possibleWrapIndex] === "\r") {
+        const newBlocks = [...blocks];
+        [newBlocks[possibleWrapIndex], newBlocks[possibleWrapIndex - 1]] = [
+          newBlocks[possibleWrapIndex - 1],
+          newBlocks[possibleWrapIndex],
+        ];
+        setBlocks(newBlocks);
+        return true;
+      } else {
+        addNewLine(possibleWrapIndex - 1, false, true);
+        return true;
+      }
+    }
 
     addNewLine(countInsertIndex(), false, true);
 
@@ -126,7 +128,7 @@ export default function BlockEditor({
       newBlocks.splice(where, 0, inputText.trim(), "\n");
       setInputText("");
     } else {
-      if(split){
+      if (split) {
         newBlocks.splice(where, 0, "\r");
       } else {
         newBlocks.splice(where, 0, "\n");
@@ -236,14 +238,21 @@ export default function BlockEditor({
 
       case "ArrowUp":
         e.preventDefault();
-        if (inputText === "" && inputLineIndex > 0) {
-          setInputLineIndex(inputLineIndex - 1);
+        moveInputUp();
 
-          // If lower line is longer, set input index to end of upper line
-          if (inputIndex > lines[inputLineIndex - 1].length) {
-            setInputIndex(lines[inputLineIndex - 1].length);
-          }
-        }
+        // requestAnimationFrame(() => {
+        //   const block = document.getElementsByClassName("line")[inputLineIndex];
+        //   console.log(block.childNodes);
+        // });
+
+        // if (inputText === "" && inputLineIndex > 0) {
+        //   setInputLineIndex(inputLineIndex - 1);
+
+        //   // If lower line is longer, set input index to end of upper line
+        //   if (inputIndex > lines[inputLineIndex - 1].length) {
+        //     setInputIndex(lines[inputLineIndex - 1].length);
+        //   }
+        // }
         break;
 
       case "ArrowDown":
@@ -275,6 +284,90 @@ export default function BlockEditor({
       default:
         break;
     }
+  }
+
+  function moveInputUp() {
+    const domLines = document.getElementsByClassName("line");
+
+    // Is the line wrapped?
+    if (domLines[inputLineIndex].clientHeight === baseLineHeight.current) {
+      if (inputText === "" && inputLineIndex > 0) {
+        setInputLineIndex(inputLineIndex - 1);
+
+        // If lower line is longer, set input index to end of upper line
+        if (inputIndex > lines[inputLineIndex - 1].length) {
+          setInputIndex(lines[inputLineIndex - 1].length);
+        }
+      }
+    } else {
+      // The line is wrapped -> find fit in line above, if input not in first line
+      const lineItems = domLines[inputLineIndex].children;
+
+      // Input on first line
+      const currentLine = lineItems[inputIndex] as HTMLElement;
+      if (currentLine.offsetTop < baseLineHeight.current) {
+
+        // Move input up
+        if (inputText === "" && inputLineIndex > 0) {
+          setInputLineIndex(inputLineIndex - 1);
+        }
+
+      } else {
+        // Input in wrapped part -> move one line up and find suitable spot
+        const inputOffset = currentLine.offsetLeft;
+        const [splitLine, inputLine] = splitLineBlocks(lineItems);
+
+        // Najit misto kde ma byt input
+        let targetIndex = (inputLine > 0 && splitLine[inputLine - 1].length > 0) ? findClosestIndex(splitLine[inputLine - 1], inputOffset) : 0;
+        if(inputLine > 0){
+            for(let i = 0; i < inputLine - 1; i++){
+                targetIndex += splitLine[i].length;
+            }
+        }
+
+        setInputIndex(targetIndex);
+      }
+    }
+  }
+
+  function findClosestIndex(values: number[], referenceValue: number){
+    let closestIndex = 0;
+    let minDiff = Math.abs(values[0] - referenceValue);
+
+    for(let i = 0; i < values.length; i++){
+        const currentDiff = Math.abs(values[i] - referenceValue);
+        if(currentDiff < minDiff){
+            minDiff = currentDiff;
+            closestIndex = i;
+        }
+    }
+
+    return closestIndex
+  }
+
+  // Returns offset left array of blocks on lines
+  function splitLineBlocks(blocks: HTMLCollection): [number[][], number] {
+    let returnArray: number[][] = [[]];
+    let lastLineOffset = blocks[0].offsetTop;
+    let index = 0;
+    let inputLine = 0;
+
+    for (const block of blocks) {
+        // console.log(block)
+      if (lastLineOffset < block.offsetTop) {
+        index++;
+        lastLineOffset = block.offsetTop;
+        returnArray[index] = [];
+      } else {
+        if(block.className === "input-box"){
+            inputLine = index;
+        }
+        
+        returnArray[index].push(block.offsetLeft);
+      }
+    }
+
+    return [returnArray, inputLine];
   }
 
   function splitLines(blocks: string[]): string[][] {
