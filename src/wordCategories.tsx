29fe8b7dc6546d3a8b231de0fd816f463c0da0bb
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2025-04-24 21:50:43 +0200

    Added ears_test in wordCategories for testing

--- src/wordCategories.tsx
+++ src/wordCategories.tsx
@@ -36,4 +36,58 @@ const ears = {
   },
 };
 
-export const languages = [ears];
+const earsTest = {
+  keywords: {
+    color: "#FFB400",
+    items: ["while", "then", "shall", "if"],
+  },
+  relationalOperators: {
+    color: "#FF6B6B",
+    items: [
+      "is",
+      "and",
+      "set",
+      "to",
+      "within",
+      "has been",
+      "for",
+      "greater than",
+    ],
+  },
+  objects: {
+    color: "#4ECDC4",
+    items: [
+      "left gear state",
+      "aircraft speed",
+      "aircraft status",
+      "operational mode",
+      "left gear visual warning",
+      "left gear warning lamp",
+    ],
+  },
+  states: {
+    color: "#1A535C",
+    items: [
+      "becomes hazardous",
+      "stops being hazardous",
+      "active",
+      "inactive",
+      "on",
+      "off",
+      "in air",
+      "on land",
+      "up and locked",
+    ],
+  },
+  actors: {
+    color: "#F7FFF7",
+    items: ["LGS Warning System"],
+  },
+  other: {
+    color: "#A9A9A9",
+    items: [",", ".", "the", "second", "seconds", "previous"],
+  },
+};
+
+
+export const languages = [ears, earsTest];
