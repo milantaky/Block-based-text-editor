e550325367a99b9d57c6da783df2b05c70fc57b5
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2025-05-04 20:55:05 +0200

    More styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import TextEditor from "./Components/TextEditor/TextEditor.tsx";
 import BlockEditor from "./Components/BlockEditor/BlockEditor.tsx";
+import SettingsWindow from "./Components/SettingsWindow/SettingsWindow.tsx";
 import type { BlockType, editorMode } from "./types";
 import "./App.css";
 
@@ -10,6 +11,11 @@ export default function App() {
   const blocksRef = useRef<BlockType[]>([]);
   const baseLineHeight = useRef<number>(0); // Height of line on first render to compare if other lines have overflown
   const blhSet = useRef<boolean>(false);
+  const [settingsVisible, setSettingsVisible] = useState(false);
+
+  function toggleSettings() {
+    setSettingsVisible(!settingsVisible);
+  }
 
   function toggleEditorMode() {
     if (editorMode === "Text") {
@@ -31,13 +37,25 @@ export default function App() {
     <div className="editor">
       <div className="editable-area">
         {editorMode === "Blocks" ? (
-          <BlockEditor text={textRef.current} blocksRef={blocksRef} baseLineHeight={baseLineHeight} blhSet={blhSet}/>
+          <BlockEditor
+            text={textRef.current}
+            blocksRef={blocksRef}
+            baseLineHeight={baseLineHeight}
+            blhSet={blhSet}
+          />
         ) : (
           <TextEditor blocks={blocksRef.current} textRef={textRef} />
         )}
       </div>
 
-      <SwitchButton />
+      {settingsVisible && <SettingsWindow/>}
+
+      <div className="main-buttons">
+        <SwitchButton />
+        <button className="settings-window-button" onClick={toggleSettings}>
+          Settings
+        </button>
+      </div>
     </div>
   );
 }
--- src/Components/SettingsWindow/SettingsWindow.tsx
+++ src/Components/SettingsWindow/SettingsWindow.tsx
@@ -1,7 +1,28 @@
-export default function SettingsWindow(){
-    return (
-        <button>
-            settings
-        </button>
-    );
+import { useState } from "react";
+
+export default function SettingsWindow() {
+  const [prefabVisible, setPrefabVisible] = useState(true);
+
+  function togglePrefab() {
+    setPrefabVisible(!prefabVisible);
+  }
+
+  return (
+    <>
+      <div className="settings-window">
+        <h3 className="settings-header">Settings</h3>
+        <div className="settings-items">
+          <div className="settings-item">
+            <input
+              className="setting-checkbox"
+              type="checkbox"
+              checked={prefabVisible}
+              onChange={togglePrefab}
+            />
+            Show prefabricated blocks
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
