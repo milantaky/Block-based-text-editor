39be9fb68909982fcab1b74261fd11218a841637
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2025-05-09 20:26:47 +0200

    Disabled spell check and autocorrect in inputbox

--- src/Components/BlockEditor/InputBox/InputBox.tsx
+++ src/Components/BlockEditor/InputBox/InputBox.tsx
@@ -13,6 +13,8 @@ export default function InputBox({
       className="input-box"
       contentEditable
       suppressContentEditableWarning
+      spellCheck={false}
+      autoCorrect="off"
       onInput={onInput}
       onKeyDown={onKeyDown}
     />
